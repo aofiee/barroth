e7ba03203f38d780b1f35ee30b502c5a686f9b9b
Khomkrid <khomkrid@twinsynergy.co.th>
2021-07-06 21:41:14 +0700

    ok

--- services/helpers/msg_helper_test.go
+++ services/helpers/msg_helper_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/aofiee/barroth/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 )
@@ -17,6 +18,20 @@ func TestMsg(t *testing.T) {
 		if err != nil {
 			assert.NotEqual(t, nil, err, err.Error())
 		}
+		return nil
+	})
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Content-Type", "application/json")
+	_, _ = app.Test(req)
+}
+func TestValidate(t *testing.T) {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		r := models.RoleItems{
+			Name:        "test",
+			Description: "test",
+		}
+		ValidateStruct(r)
 		return nil
 	})
 	req := httptest.NewRequest("GET", "/", nil)
